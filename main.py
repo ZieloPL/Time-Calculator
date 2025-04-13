abd526b7be48a0a27f8c38c4dd79afc473af6bbe
Jan Zielo <janzielinski117@gmail.com>
2025-04-13 17:03:18 +0200

    Refactoring

--- main.py
+++ main.py
@@ -1,61 +1,58 @@
 def add_time(start, duration, day_of_week=''):
-    hours = int(start.split(':')[0])
-    minutes = int(start.split(':')[1][0:2])
-    time_of_day = start[-2]
+    # Parse start time
+    start_hour, start_minute = map(int, start[:-3].split(':'))
+    period = start[-2]
 
+    # Parse duration
+    duration_hour, duration_minute = map(int, duration.split(':'))
+
+    # Days of the week
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    new_day = ''
-    days_later = 0
-    message = ''
 
-    new_hours = int(duration.split(':')[0])
-    new_minutes = int(duration.split(':')[1])
+    # Calculate total minutes
+    total_minutes = start_minute + duration_minute
+    extra_hours = total_minutes // 60
+    new_minutes = total_minutes % 60
 
-    if new_hours + hours >= 24:
-        days_later = (new_hours + hours) // 24
-        new_hours += hours - days_later * 24
-    else:
-        new_hours += hours
+    # Calculate total hours
+    total_hours = start_hour + duration_hour + extra_hours
+    days_later = total_hours // 24
+    final_hour_24 = total_hours % 24
 
-    if minutes + new_minutes >= 60:
-        if new_hours == 11 and time_of_day == 'P':
-            time_of_day = 'A'
-            days_later += 1
-        elif new_hours == 11 and time_of_day == 'A':
-            time_of_day = 'P'
-        new_hours += (minutes + new_minutes) // 60
-        new_minutes = (minutes + new_minutes) % 60
-    else:
-        new_minutes += minutes
+    # Determine AM/PM switch
+    period_switches = ((start_hour % 12 + duration_hour + extra_hours) // 12) % 2
+    if period == 'P' and (start_hour % 12 + duration_hour + extra_hours) // 12 >= 1:
+        days_later += 1
+    if (start_hour % 12 + duration_hour + extra_hours) // 12 >= 1:
+        period = 'A' if period == 'P' else 'P'
+    period = 'AM' if period == 'A' else 'PM'
 
-    if new_minutes <= 9:
-        new_minutes = '0' + str(new_minutes)
+    # Convert 24-hour format to 12-hour
+    display_hour = final_hour_24 % 12
+    if display_hour == 0:
+        display_hour = 12
 
-    if new_hours > 12:
-        if time_of_day == 'P':
-            days_later += 1
-        time_of_day = 'AM' if time_of_day == 'P' else 'PM'
-        new_hours -= 12
-    else:
-        time_of_day += 'M'
+    # Format minutes
+    display_minute = f'{new_minutes:02}'
 
+    # Handle day of the week (if provided)
+    day_info = ''
+    if day_of_week:
+        day_index = days.index(day_of_week.strip().capitalize())
+        new_day = days[(day_index + days_later) % 7]
+        day_info = f', {new_day}'
 
+    # Handle days later message
     if days_later == 1:
-        message = ' (next day)'
+        later_info = ' (next day)'
     elif days_later > 1:
-        message = f' ({days_later} days later)'
+        later_info = f' ({days_later} days later)'
     else:
-        message = ''
+        later_info = ''
 
-
-    new_time = str(new_hours) + ':' + str(new_minutes) + ' ' + time_of_day
-
-    if day_of_week != '':
-        new_day = ', '+ days[(days.index(day_of_week.casefold().capitalize()) + days_later) % 7]
-        new_time += new_day
-
-
-    new_time += message
+    # Build the final time string
+    new_time = f"{display_hour}:{display_minute} {period}{day_info}{later_info}"
     return new_time
+
 
 print(add_time('8:16 PM', '466:02', 'tuesday'))
